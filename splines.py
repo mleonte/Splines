bbb33d581cbebbd108ebae9ebae7b4dcd9f8d7ef
Monica leonte <monica.leonte@mail.utoronto.ca>
2018-04-29 16:39:09 -0400

    -Fixed hermite splines

--- splines.py
+++ splines.py
@@ -35,26 +35,31 @@ def hermiteSplines(pts):
     plt.figure()
     plt.title("Hermite Splines")
     numPts = pts.shape[1]
-    colours = ["r-", "g-", "b-"]
     
-    delta = 0.1
+    delta = 0.001
     
     for i in range(0, pts.shape[1], 4):
         plt.scatter(pts[0,i:i + 2], pts[1,i:i + 2])
-        mat = np.array([[0, 0, 0, 1],
+        mat = np.array([[1, 0, 0, 0],
                         [1, 1, 1, 1],
-                        [0, 0, 1, 0],
-                        [3, 2, 1, 0]])
+                        [0, 1, 0, 0],
+                        [0, 1, 2, 3]])
     
         coeff = np.matmul(np.linalg.inv(mat), pts[:,i:i + 4].T)
-        print coeff
-        t_mat = np.array([[(delta * k) ** j for j in range(numPts)]
+        t_mat = np.array([[(delta * k) ** j for j in range(4)]
                           for k in range(int(1/delta) + 1)])
         
-        print t_mat
-        x = np.matmul(t_mat, coeff)
-        print x
-        plt.plot(x[:,0], x[:,1], colours[i % 3])
+        x = np.matmul(t_mat, coeff).T
+        plt.plot(x[0], x[1])
+        
+        toPlot = ([pts[0, i] - pts[0, i + 2], pts[0, i], pts[0, i] + pts[0, i + 2]],
+                  [pts[1, i] - pts[1, i + 2], pts[1, i], pts[1, i] + pts[1, i + 2]])
+        plt.plot(toPlot[0], toPlot[1])
+        
+        toPlot = ([pts[0, i + 1] - pts[0, i + 3], pts[0, i + 1], pts[0, i + 1] + pts[0, i + 3]],
+                  [pts[1, i + 1] - pts[1, i + 3], pts[1, i + 1], pts[1, i + 1] + pts[1, i + 3]])
+        plt.plot(toPlot[0], toPlot[1])
+        
     
     plt.savefig("./Plots/HermiteSplines.png", dpi=300) 
     
@@ -65,7 +70,7 @@ def bezierCurve(pts):
     plt.scatter(pts[0], pts[1])
     numPts = pts.shape[1]
     
-    delta = 0.1
+    delta = 0.001
     
     t = [delta * i for i in range(int(1/delta) + 1)]
     c_t = []
@@ -73,7 +78,6 @@ def bezierCurve(pts):
         c_t.append(sum([pts[:,j] * bernsteinTerm(numPts - 1, j, i) for j in range(numPts)]))
     
     c_t = np.array(c_t)
-    print c_t
     
     plt.plot(c_t[:,0], c_t[:,1], 'r-')
         
@@ -89,19 +93,19 @@ pts = np.array(
     [[1, 2, 3, 4, 6, 8, 10, 12],
      [1, -2, 3, -4, 6, 7, 3, 1]])    
 
-#interpolate(pts)
+interpolate(pts)
 
 pts = np.array(
-    [[1, 2, 3],
-     [1, -2, 3]])  
+    [[0,  5, 1, 0, 5, 6, 0, 1],
+     [0,  5, 0, 1, 5, 10, 1, -1]]) 
+
+hermiteSplines(pts)
+
+pts = np.array(
+    [[1, 2, 3, 4, 7, 8],
+     [1, -2, 3, 2, 1, 7]])  
 
 bezierCurve(pts)
-
-#pts = np.array(
-    #[[0,  1, 0, 1],
-     #[0,  1, -1, 0]]) 
-
-#hermiteSplines(pts)
 
 
     
